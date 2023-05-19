4ba1ee11ff76cc9448e72a0298996e9eff2d8b34
Isaac <icorbrey@gmail.com>
2023-05-19 15:54:52 -0400

    feat: Set up React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { routes } from 'routes';
 import React from 'react';
+
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
+		<RouterProvider {...{ router }} />
 	</React.StrictMode>,
 );
